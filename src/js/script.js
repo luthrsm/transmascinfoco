db2c66e7d5134fa7a637fe330b138394940675c7
nicolas <nicosantosmaia18@gmail.com>
2025-04-05 01:49:43 -0300

    esqueleto home e posts

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true, 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
